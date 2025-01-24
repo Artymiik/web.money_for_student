8a6e6ad9de8fe2b0fa9dadf32408a1de3976b96b
Artymiik <aworkprogramm@gmail.com>
2025-01-24 12:08:22 +0500

    added json files (content)

---
+++ content/footer.ts
@@ -0,0 +1,45 @@
+export const footer = [
+  {
+    id: 1,
+    title: "Несовершеннолетние работники: особенности найма и ограничения",
+    graph: "Контур: [сайт]. – 2023. – URL:",
+    url: "https://kontur.ru/articles/5270",
+    timestamp: "(дата обращения: 20.10.2024)",
+  },
+  {
+    id: 2,
+    title: "Как заработать деньги школьнику: 60 идей – сравни:",
+    graph: "Контур: [сайт]. – 2023. - URL",
+    url: "https://www.sravni.ru/text/kak-zarabotat-dengi-shkolniku-60-idej/?upd=true",
+    timestamp: "(дата обращения: 25.10.2024)",
+  },
+  {
+    id: 3,
+    title:
+      "На чем зарабатывать подростку: обзор доступных вариантов заработка | Блог 4brain – 4BRAIN:",
+    graph: "Контур: [сайт]. – 2023. – URL:",
+    url: "https://4brain.ru/blog/na-chem-zarabatyvat-podrostku-obzor-dostupnykh-variantov-zarabotka",
+    timestamp: "(дата обращения: 25.10.2024)",
+  },
+  {
+    id: 4,
+    title: "Работа для подростка: плюсы и минусы:",
+    graph: "Контур: [сайт]. – 2023. – URL:",
+    url: "https://medaboutme.ru/articles/rabota_dlya_podrostka_plyusy_i_minusy/?ysclid=m5wfv6fei69469275",
+    timestamp: "(дата обращения: 01.11.2024)",
+  },
+  {
+    id: 5,
+    title: "Стоит ли подростку 14-17 лет работать: плюсы и минусы:",
+    graph: "Контур: [сайт]. – 2022. – URL:",
+    url: "https://www.proaist.ru/articles/stoit-li-podrostku-14-17-let-rabotat-plyusy-i-minusy/?ysclid=m5wh3817tc39697281",
+    timestamp: "(дата обращения: 05.11.2024)",
+  },
+  {
+    id: 6,
+    title: "Несовершеннолетние работники: особенности найма и ограничения",
+    graph: "Контур: [сайт]. – 2023. – URL:",
+    url: "https://kontur.ru/articles/5270",
+    timestamp: "(дата обращения: 20.10.2024)",
+  },
+];
