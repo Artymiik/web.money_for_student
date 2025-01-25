c02cc829c53e6d159a8c9c94d59e2b540b2a1db8
Artymiik <aworkprogramm@gmail.com>
2025-01-25 14:33:59 +0500

    changed loc

---
+++ src/constants/Image.ts
@@ -0,0 +1,5 @@
+const href_base = "./";
+
+const Images = {
+  minus_plus: require(`${href_base}/plus_minus.jpg`),
+};
--- src/hooks/useLogLocation.ts
+++ src/hooks/useLogLocation.ts
@@ -3,22 +3,27 @@ import axios from "axios";
 
 export default async function useLogLocation(mutateLog: ReactMutation<any>) {
   const host = `${window.location.protocol}//${window.location.host}`;
+  const api_key = import.meta.env.VITE_API_KEY_IP_1
+    ? import.meta.env.VITE_API_KEY_IP_1
+    : import.meta.env.VITE_API_KEY_IP_2;
 
   try {
-    const response = await axios.get("http://ip-api.com/json");
+    const response = await axios.get(
+      `https://api.ipgeolocation.io/ipgeo?apiKey=${api_key}`
+    );
 
     mutateLog({
       status: "info",
       host: host,
-      country: response.data.country,
-      countryCode: response.data.countryCode,
-      region: response.data.region,
-      regionName: response.data.regionName,
+      country: response.data.country_name,
+      countryCode: response.data.country_code2,
+      region: response.data.state_prov,
+      regionName: response.data.country_name_official,
       city: response.data.city,
-      zip: response.data.zip,
-      lat: response.data.lat,
-      lon: response.data.lon,
-      query: response.data.query,
+      zipcode: response.data.zipcode,
+      latitude: response.data.latitude,
+      longitude: response.data.longitude,
+      query: response.data.ip,
     });
   } catch (error) {
     console.log(error);
